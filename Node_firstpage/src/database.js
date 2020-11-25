eae2e046f57f167217de531fee0a00178a4078e9
Joelalex96 <75039783+Joelalex96@users.noreply.github.com>
2020-11-25 12:30:49 -0600

    subir

---
+++ Node_firstpage/src/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect(
+    'mongodb+srv://Esli:Esli15121995@cluster0.q21gi.mongodb.net/test?retryWrites=true', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+.then(db => console.log('Base de datos conectada'))
+.catch(err => console.error(err));
